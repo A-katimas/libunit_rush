63a3db685136f542aeefc23fa8fec077f8968e86
Veliga Syutkin <vsyutkin@k2r1p5.42mulhouse.fr>
2025-11-24 22:01:40 +0100

    WIP

--- real_test/test.h
+++ real_test/test.h
@@ -3,24 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   test.h                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jtardieu <jtardieu@student.42.fr>          +#+  +:+       +#+        */
+/*   By: vsyutkin <vsyutkin@student.42mulhouse.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/24 15:22:01 by jtardieu          #+#    #+#             */
-/*   Updated: 2025/11/24 21:39:28 by jtardieu         ###   ########.fr       */
+/*   Updated: 2025/11/24 21:59:55 by vsyutkin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TEST_H
-#define TEST_H
+# define TEST_H
 
 
-#include "../resource/libft.h"
+# include "../resource/libft.h"
+# include "../1_LIBUNIT/libunit.h"
 
-int	teststrlen1();
-int	teststrlen2();
-int	teststrlen3();
-int	teststrlen4();
+int	teststrlen1(void);
+int	teststrlen2(void);
+int	teststrlen3(void);
+int	teststrlen4(void);
 
+int	test_atoi1(void);
+int	test_atoi2(void);
+int	test_atoi3(void);
+int	test_atoi4(void);
+
+void	strlen_launcher(void);
 int	test_atoi1();
 int	test_atoi2();
 int	test_atoi3();
