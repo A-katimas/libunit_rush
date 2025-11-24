0b3b32d2d7bda2e059516cb7428fa28bc865aedb
Veliga Syutkin <vsyutkin@k2r1p5.42mulhouse.fr>
2025-11-24 21:54:02 +0100

    Sauvegarde avant merge

--- real_test/main.c
+++ real_test/main.c
@@ -3,22 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jtardieu <jtardieu@student.42.fr>          +#+  +:+       +#+        */
+/*   By: vsyutkin <vsyutkin@student.42mulhouse.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/24 15:18:26 by jtardieu          #+#    #+#             */
-/*   Updated: 2025/11/24 20:25:59 by jtardieu         ###   ########.fr       */
+/*   Updated: 2025/11/24 21:23:29 by vsyutkin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "test.h"
 
-int main(int ac,char **av)
+int	main(void)
 {
-	int a= 0;
-	ft_printf("argu %d str %s \n",ac,av[1]);
-	a = teststrlen1();
-	if (!a)
-		ft_printf("yes");
-	else
-		ft_printf("nan0");
+	// int	a;
+
+	// a = 0;
+	// ft_printf("argu %d str %s \n", ac, av[1]);
+	// a = teststrlen1();
+	// if (!a)
+	// 	ft_printf("yes");
+	// else
+	// 	ft_printf("nan0");
+
+	strlen_launcher();
 }
