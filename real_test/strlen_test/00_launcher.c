d0750ceb5e2fc5e5b3fd210e855807bcf564b5ec
Veliga Syutkin <vsyutkin@k1r3p8.42mulhouse.fr>
2025-11-25 14:49:14 +0100

    fix: test name

--- real_test/strlen_test/00_launcher.c
+++ real_test/strlen_test/00_launcher.c
@@ -6,7 +6,7 @@
 /*   By: vsyutkin <vsyutkin@student.42mulhouse.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/24 20:41:13 by vsyutkin          #+#    #+#             */
-/*   Updated: 2025/11/25 14:22:55 by vsyutkin         ###   ########.fr       */
+/*   Updated: 2025/11/25 14:48:57 by vsyutkin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@ void	strlen_launcher(void)
 	t_unit_test	*test_list;
 
 	test_list = NULL;
-	load_test(&test_list, "ft_strlen test 1", &teststrlen1, TEST_SUCCESS);
-	load_test(&test_list, "ft_strlen test 2", &teststrlen2, TEST_SUCCESS);
-	load_test(&test_list, "ft_strlen test 3", &teststrlen3, TEST_SUCCESS);
-	load_test(&test_list, "ft_strlen test 4", &teststrlen4, TEST_SUCCESS);
+	load_test(&test_list, "STRLEN: test 1", &teststrlen1, TEST_SUCCESS);
+	load_test(&test_list, "STRLEN: test 2", &teststrlen2, TEST_SUCCESS);
+	load_test(&test_list, "STRLEN: test 3", &teststrlen3, TEST_SUCCESS);
+	load_test(&test_list, "STRLEN: test 4", &teststrlen4, TEST_SUCCESS);
 
 	launch_tests(&test_list);
 	display_test_result(&test_list);
