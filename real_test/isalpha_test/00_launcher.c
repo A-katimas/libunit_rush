706f7f5d5e6d27e62d4d10c1c07f0be6cb8e866a
Veliga Syutkin <veliga99@gmail.com>
2025-11-26 08:53:10 +0100

    chore: cleanup

--- real_test/isalpha_test/00_launcher.c
+++ real_test/isalpha_test/00_launcher.c
@@ -3,25 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   00_launcher.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jtardieu <jtardieu@student.42.fr>          +#+  +:+       +#+        */
+/*   By: vsyutkin <vsyutkin@student.42mulhouse.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/24 20:41:13 by vsyutkin          #+#    #+#             */
-/*   Updated: 2025/11/25 17:43:23 by jtardieu         ###   ########.fr       */
+/*   Updated: 2025/11/26 08:49:32 by vsyutkin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../test.h"
 
-void	strlen_launcher(void)
+void	isalpha_launcher(void)
 {
 	t_unit_test	*test_list;
 
 	test_list = NULL;
-	load_test(&test_list, "STRLEN: test 1", &test_isalpha1, TEST_SUCCESS);
-	load_test(&test_list, "STRLEN: test 2", &test_isalpha2, TEST_SUCCESS);
-	load_test(&test_list, "STRLEN: test 3", &test_isalpha3, TEST_SUCCESS);
-	load_test(&test_list, "STRLEN: test 4", &test_isalpha4, TEST_SUCCESS);
-	load_test(&test_list, "STRLEN: test 4", &test_isalpha5, TEST_SUCCESS);
+	load_test(&test_list, "ISALPHA: test 1", &test_isalpha1, TEST_SUCCESS);
+	load_test(&test_list, "ISALPHA: test 2", &test_isalpha2, TEST_SUCCESS);
+	load_test(&test_list, "ISALPHA: test 3", &test_isalpha3, TEST_SUCCESS);
+	load_test(&test_list, "ISALPHA: test 4", &test_isalpha4, TEST_SUCCESS);
+	load_test(&test_list, "ISALPHA: test 4", &test_isalpha5, TEST_SUCCESS);
 	launch_tests(&test_list);
 	display_test_result(&test_list);
 	cleanup_test_list(&test_list);
--- real_test/str_join_test/00_launcher.c
+++ real_test/str_join_test/00_launcher.c
@@ -3,24 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   00_launcher.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jtardieu <jtardieu@student.42.fr>          +#+  +:+       +#+        */
+/*   By: vsyutkin <vsyutkin@student.42mulhouse.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/24 20:41:13 by vsyutkin          #+#    #+#             */
-/*   Updated: 2025/11/25 17:51:51 by jtardieu         ###   ########.fr       */
+/*   Updated: 2025/11/26 08:48:37 by vsyutkin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../test.h"
 
-void	strlen_launcher(void)
+void	strjoin_launcher(void)
 {
 	t_unit_test	*test_list;
 
 	test_list = NULL;
-	load_test(&test_list, "STRLEN: test 1", &test_strjoin1, TEST_SUCCESS);
-	load_test(&test_list, "STRLEN: test 2", &test_strjoin2, TEST_SUCCESS);
-	load_test(&test_list, "STRLEN: test 3", &test_strjoin3, TEST_SUCCESS);
-	load_test(&test_list, "STRLEN: test 4", &test_strjoin4, TEST_SUCCESS);
+	load_test(&test_list, "STRJOIN: test 1", &test_strjoin1, TEST_SUCCESS);
+	load_test(&test_list, "STRJOIN: test 2", &test_strjoin2, TEST_SUCCESS);
+	load_test(&test_list, "STRJOIN: test 3", &test_strjoin3, TEST_SUCCESS);
+	load_test(&test_list, "STRJOIN: test 4", &test_strjoin4, TEST_SUCCESS);
 	launch_tests(&test_list);
 	display_test_result(&test_list);
 	cleanup_test_list(&test_list);
